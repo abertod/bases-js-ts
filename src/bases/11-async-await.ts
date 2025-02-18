bcd78c6bce1be55fce2373542c46b13e6864e006
master <master@dib-e026-imac20.dib.upv.es>
2025-02-18 18:42:26 +0100

    Fetch a una api

---
+++ src/bases/11-async-await.ts
@@ -0,0 +1,8 @@
+// import { GIFResponsio } from "../interfaces/gif.responsio";
+// import {}
+
+// export const obtineImago = async() => {
+//     const resp = await giphyApi.get<GIFResponsio>('/random');
+// }
+
+// console.log(obtineImago());
